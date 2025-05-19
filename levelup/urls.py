da9ca156c82d8c0314bd351193583f52ac42f6e9
Mike McGee <mmcgee738@gmail.com>
2025-05-19 18:50:42 -0500

    game types respond to get requests now

--- levelup/urls.py
+++ levelup/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
+from levelupapi.views import GameTypeView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypeView, 'gametype')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
